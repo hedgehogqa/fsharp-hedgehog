52fe2987af2d78ca4395e9d20554b78e06994905
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-10-08 14:28:03 +0300

    Replace MONO in build.sh with a less specific name, in order to make it clear that the build isn't always triggered via Mono.

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -eu
 
 if [ -z "${COMSPEC-}" ]; then
-  MONO="mono"
+  run="mono"
 else
-  MONO=""
+  run=""
 fi
 
 # Only run bootstrapper if we haven't done so in the last 24 hours, this keeps builds fast.
 if ! test -f .paket/paket.bootstrapper.run || find .paket/paket.bootstrapper.run -type f -mtime +1 | grep -q paket.bootstrapper.run; then
-    $MONO .paket/paket.bootstrapper.exe
+    $run .paket/paket.bootstrapper.exe
     touch .paket/paket.bootstrapper.run
 fi
 
-$MONO .paket/paket.exe restore
-$MONO packages/FAKE/tools/FAKE.exe $@
+$run .paket/paket.exe restore
+$run packages/FAKE/tools/FAKE.exe $@
