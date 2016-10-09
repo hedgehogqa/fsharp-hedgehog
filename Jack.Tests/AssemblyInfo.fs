af3796a85182ad2cde5408a5a24ed624e66165ad
Jacob Stanley <jacob@stanley.io>
2016-10-09 11:38:23 +1100

    Rename Jack.UnitTests -> Jack.Tests

--- Jack.Tests/AssemblyInfo.fs
+++ Jack.Tests/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace Jack.UnitTests.AssemblyInfo
+﻿namespace Jack.Tests.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("Jack.UnitTests")>]
+[<assembly: AssemblyTitle("Jack.Tests")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Jack.UnitTests")>]
+[<assembly: AssemblyProduct("Jack.Tests")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
--- Jack.Tests/Library1.fs
+++ Jack.Tests/Library1.fs
@@ -1,4 +1,4 @@
-﻿namespace Jack.UnitTests
+﻿namespace Jack.Tests
 
 type Class1() = 
     member this.X = "F#"
