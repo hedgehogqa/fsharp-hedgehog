1b400ceb39e8e4f3d3eacdea79ec9dd0c88d7c80
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2016-10-11 11:43:14 +0300

    Add an example-based test for Shrink.halves,
    which uses plain old xUnit.net data theories, with the data coming from
    inline values.
    
    Even though Shrink.halves is an overloaded function, this example-based
    test currently exercises it with integers.
    
    A way of writing an overloaded example-based test against Shrink.halves
    could probably be via some sort of an Abstract Class and having derived
    classes providing the implementations.

--- Jack.Tests/ShrinkTests.fs
+++ Jack.Tests/ShrinkTests.fs
@@ -15,3 +15,27 @@ let ``removes permutes a list by removing 'k' consecutive elements from it``() =
                           [ 1; 2; 3; 4 ] ]
     // http://stackoverflow.com/a/17101488
     test <@ Seq.fold (&&) true (Seq.zip expected actual |> Seq.map (fun (a, b) -> a = b)) @>
+
+[<Theory>]
+[<InlineData(-4096)>]
+[<InlineData(-2048)>]
+[<InlineData(   -8)>]
+[<InlineData(   -1)>]
+[<InlineData(    0)>]
+[<InlineData(    1)>]
+[<InlineData(    2)>]
+[<InlineData(    3)>]
+[<InlineData(   30)>]
+[<InlineData(  128)>]
+[<InlineData(  256)>]
+[<InlineData( 8192)>]
+[<InlineData(10240)>]
+let ``halves Produces a list containing the results of halving a number`` n =
+    let actual = Shrink.halves n |> LazyList.toList
+
+    let expected =
+        n |> List.unfold (fun x ->
+                 match x with
+                 | 0 -> None
+                 | _ -> Some (x, x / 2))
+    expected =! actual
