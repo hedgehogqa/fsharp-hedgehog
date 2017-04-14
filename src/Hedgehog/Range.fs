94b960df10bd2f1c3b7cdf05dcac820d519cd6d1
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2017-04-14 06:39:30 +0300

    Add Range type.

---
+++ src/Hedgehog/Range.fs
@@ -0,0 +1,14 @@
+﻿namespace Hedgehog
+
+/// A range describes the bounds of a number to generate, which may or may not
+/// be dependent on a 'Size'.
+type Range<'a> =
+    | Range of ('a * (Size -> 'a * 'a))
+
+module Range =
+    let private bimap (f : 'a -> 'b) (g : 'c -> 'd) (a : 'a, b : 'c) : 'b * 'd =
+        f a, g b
+
+    let map (f : 'a -> 'b) (Range (z, g) : Range<'a>) : Range<'b> =
+        Range (f z, fun sz ->
+            bimap f f (g sz))
---
+++ tests/Hedgehog.Tests/RangeTests.fs
@@ -0,0 +1,1 @@
+﻿module Hedgehog.Tests.RangeTests
