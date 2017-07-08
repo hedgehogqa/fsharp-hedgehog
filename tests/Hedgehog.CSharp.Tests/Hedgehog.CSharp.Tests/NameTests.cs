aa03bb9cad89360d20f1eab7d03a4137f5d1b67f
George Pollard <porges@porg.es>
2017-07-08 11:51:26 +1000

    Add tests for (compiled) core API to conform to Framework guidelines
    This is so it will be ergonomic to use from C#.
    
    At the moment we just check that the name conforms to the Framework
    Guidlines by starting with a capital letter.
    
    Gen and Range are considered the 'core' APIs at the moment.

---
+++ tests/Hedgehog.CSharp.Tests/Hedgehog.CSharp.Tests/NameTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Hedgehog.CSharp.Tests
+{
+    public class NameTests
+    {
+        private static Type[] _publicApiTypes =
+            { typeof(Gen)
+            , typeof(Range)
+            , typeof(Property)
+            };
+
+        public static IEnumerable<MemberInfo[]> AllPublicMembers()
+        {
+            var bindingFlags =
+                BindingFlags.Public |
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.DeclaredOnly;
+
+            foreach (var type in _publicApiTypes)
+            {
+                var members = type.GetMembers(bindingFlags);
+
+                foreach (var member in members)
+                {
+                    // Ignore special members like indexers etc.
+                    if (member is PropertyInfo p && p.IsSpecialName)
+                    {
+                        continue;
+                    }
+
+                    if (member is MethodInfo m &&
+                        (m.IsSpecialName || m.IsConstructor || m.Name.StartsWith("get_")))
+                    {
+                        continue;
+                    }
+
+                    yield return new [] { member };
+                }
+            }
+
+        }
+
+        [Theory]
+        [MemberData(nameof(AllPublicMembers))]
+        public void AllPublicMembersFollowDotNetNamingGuidelines(MemberInfo mi)
+        {
+            var startsWithUppercaseChar = char.IsUpper(mi.Name.First());
+            Assert.True(startsWithUppercaseChar, $"{mi.Name} should start with uppercase letter");
+        }
+    }
+}
