f447a608ac4f4d8e7cde366faa01846fef187792
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2020-04-17 14:00:22 +0200

    Housekeeping
    * .gitignore takes Fable into account
    * Fable.Tests/ become Hedgehog.Fable.Tests (as with Hedgehog.CSharp.Tests)
    * Webpack isn't required (see fable-splitter's individual JS files)
    * 'npm install && npm run build' do the same as compile-with-fable script

--- tests/Fable.Tests/compile-with-fable.sh
+++
@@ -1,5 +0,0 @@
-# to install npm:
-# apt-get update && apt-get install npm
-
-npm install
-npm run build
--- tests/Hedgehog.Fable.Tests/Tests.fs
+++ tests/Hedgehog.Fable.Tests/Tests.fs
@@ -4,7 +4,7 @@ open Hedgehog
 
 [<EntryPoint>]
 let main args =
-    // For now just test if everything compiles
+    // For now just test if everything compiles.
     property {
         let! x = Gen.int (Range.linearBounded())
         return x = x
