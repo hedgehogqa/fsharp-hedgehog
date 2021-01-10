b9a317517326e6ffc1c416906d26260133875415
Adam Becker <im.adam.becker@gmail.com>
2021-01-10 23:52:59 +0000

    Rename tuple methods

--- src/Hedgehog/Tuple.fs
+++ src/Hedgehog/Tuple.fs
@@ -2,18 +2,18 @@ namespace Hedgehog
 
 module private Tuple =
 
-    let mapFirst (f : 'a -> 'c) (x : 'a, y : 'b) : 'c * 'b =
+    let mapFst (f : 'a -> 'c) (x : 'a, y : 'b) : 'c * 'b =
         f x, y
 
-    let mapSecond (f : 'b -> 'c) (x : 'a, y : 'b) : 'a * 'c =
+    let mapSnd (f : 'b -> 'c) (x : 'a, y : 'b) : 'a * 'c =
         x, f y
 
 
 module private GenTuple =
 
-    let mapFirst (f : 'a -> 'c) (gen : Gen<'a * 'b>) : Gen<'c * 'b> =
-        Gen.map (Tuple.mapFirst f) gen
+    let mapFst (f : 'a -> 'c) (gen : Gen<'a * 'b>) : Gen<'c * 'b> =
+        Gen.map (Tuple.mapFst f) gen
 
 
-    let mapSecond (f : 'b -> 'c) (gen : Gen<'a * 'b>) : Gen<'a * 'c> =
-        Gen.map (Tuple.mapSecond f) gen
+    let mapSnd (f : 'b -> 'c) (gen : Gen<'a * 'b>) : Gen<'a * 'c> =
+        Gen.map (Tuple.mapSnd f) gen
