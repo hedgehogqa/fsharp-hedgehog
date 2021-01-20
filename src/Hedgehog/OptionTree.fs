2fb42a8578e5611fbe278caed0377a6811c2cdff
Tyson Williams <tyson.williams@blocherconsulting.com>
2021-01-20 06:37:40 -0600

    added OptionTree.traverse

---
+++ src/Hedgehog/OptionTree.fs
@@ -0,0 +1,11 @@
+﻿[<RequireQualifiedAccess>]
+module internal Hedgehog.OptionTree
+
+
+let traverse (f: 'a -> Tree<'b>) (maybeTree: 'a option) : Tree<'b option> =
+  match maybeTree with
+  | None -> None |> Tree.singleton
+  | Some t -> t |> f |> Tree.map Some
+
+let sequence (maybeTree: Tree<'a> option) : Tree<'a option> =
+  traverse id maybeTree
