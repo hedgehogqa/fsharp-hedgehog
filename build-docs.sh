a6b43cad5d0068073f0dd088f75317a5c7aecd5f
adam-becker <47185607+adam-becker@users.noreply.github.com>
2021-01-22 13:24:01 -0700

    Initial working fsdocs (#277)
    * Initial working fsdocs
    
    * Remove directory props file
    
    * Add script to publish fsdocs
    
    * Fix script
    
    * Rename tutorial file to index.md
    
    * Update build-docs.sh
    
    Co-authored-by: Nikos Baxevanis <nikos.baxevanis@gmail.com>
    
    Co-authored-by: Nikos Baxevanis <nikos.baxevanis@gmail.com>

---
+++ build-docs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -ex
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+    echo "Expected a version as the first argument."
+    exit 1
+fi
+
+# Build doc files, they appear in ./output.
+
+CONFIGURATION=Release
+FRAMEWORK=netstandard2.0
+INPUT_DIR="./doc"
+OUTPUT_DIR="./output"
+
+dotnet build -c $CONFIGURATION -f $FRAMEWORK ./src/Hedgehog
+
+dotnet fsdocs build \
+    --input $INPUT_DIR \
+    --property Configuration=$CONFIGURATION,TargetFramework=$FRAMEWORK \
+    --strict
+
+# Get the artifacts into the `gh-pages` branch.
+
+REPO_BRANCH="gh-pages"
+REPO_URL="git@github.com:hedgehogqa/fsharp-hedgehog.git"
+TEMP_DIR="temp/$REPO_BRANCH"
+
+# Remove temp directory.
+
+rm -rf $TEMP_DIR
+mkdir -p $TEMP_DIR
+
+# Clone our repo's `gh-pages` branch into the temp directory.
+
+pushd $TEMP_DIR
+
+git clone -b $REPO_BRANCH $REPO_URL .
+
+find . -maxdepth 1 ! -path '*.git*' ! -path . -exec rm -rf {} \;
+
+popd
+
+# Copy all artifacts into the temp directory, and commit.
+
+cp -r $OUTPUT_DIR/* $TEMP_DIR
+
+pushd $TEMP_DIR
+
+git add .
+git commit -m "Update generated documentation for version $VERSION"
+git push # `gh-pages` should be a tracking branch for origin/gh-pages.
+
+popd
