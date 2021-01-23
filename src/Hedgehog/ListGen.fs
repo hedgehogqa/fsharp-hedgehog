4f681063e33d8d3e6b881715d3f480b9614584c2
Tyson Williams <34664007+TysonMN@users.noreply.github.com>
2021-01-23 14:35:29 -0600

    add ListGen.traverse (#260)
    * added ListGen.traverse
    
    * added failing test
    
    * Revert "added failing test"
    
    This reverts commit 762d5adb7d7ca2b513762f4167df3dc1e68de174.

---
+++ src/Hedgehog/ListGen.fs
@@ -0,0 +1,17 @@
+﻿namespace Hedgehog
+
+module ListGen =
+
+    let traverse (f: 'a -> Gen<'b>) (ma: list<'a>) : Gen<list<'b>> =
+        let rec loop input output =
+            match input with
+            | [] -> output |> List.rev |> Gen.constant
+            | a :: input ->
+                gen {
+                    let! b = f a
+                    return! loop input (b :: output)
+                }
+        loop ma []
+
+    let sequence (gens : List<Gen<'a>>) : Gen<List<'a>> =
+        gens |> traverse id
