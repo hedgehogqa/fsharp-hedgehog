0f7639dc9d57f7ee59831a7f0bb310ec3f60d9a2
Tyson Williams <tyson.d.w@gmail.com>
2021-02-24 15:00:37 -0600

    added Report.Render() to the Linq API

---
+++ src/Hedgehog/Linq/Report.fs
@@ -0,0 +1,16 @@
+﻿namespace Hedgehog.Linq
+
+#if !FABLE_COMPILER
+
+open System.Runtime.CompilerServices
+open Hedgehog
+
+[<Extension>]
+[<AbstractClass; Sealed>]
+type ReportExtensions private () =
+
+    [<Extension>]
+    static member Render (report: Report) : string =
+        Report.render report
+
+#endif
