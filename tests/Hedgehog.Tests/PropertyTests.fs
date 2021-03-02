c6f1742f7dd4f7cca59e3be7eaff6ff5a228cc72
Alex T <altxt@yandex.ru>
2021-03-03 03:27:55 +0500

    Applied changes recommended in review

--- tests/Hedgehog.Tests/PropertyTests.fs
+++ tests/Hedgehog.Tests/PropertyTests.fs
@@ -6,16 +6,11 @@ open TestDsl
 
 let propertyTests = testList "Property tests" [
     fableIgnore "generated C# list of five elements is not abbreviated in the failure report" <| fun _ ->
-        Expect.throwsC
-            (fun () ->
-                property {
-                    let! xs = Range.singleton 0 |> Gen.int |> Gen.list (Range.singleton 5) |> Gen.map ResizeArray
-                    return Seq.forall ((>) 0) xs
-                }
-                |> Property.checkWith (PropertyConfig.withShrinks 0<shrinks> PropertyConfig.defaultConfig)
-            )
-            (fun ex ->
-                Expect.isNotMatch ex.Message "\.\.\." "Abbreviation (...) found"
-            )
+        let prop = property {
+            let! xs = Range.singleton 0 |> Gen.int |> Gen.list (Range.singleton 5) |> Gen.map ResizeArray
+            return false
+        }
+        let report = Property.renderWith (PropertyConfig.withShrinks 0<shrinks> PropertyConfig.defaultConfig) prop
+        Expect.isNotMatch report "\.\.\." "Abbreviation (...) found"
 
 ]
