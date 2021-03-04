a265eea6faf0a6932690aee46bd62a0653a2ae5f
Alex T <altxt@yandex.ru>
2021-03-04 22:41:40 +0500

    Used forward pipe in test

--- tests/Hedgehog.Tests/PropertyTests.fs
+++ tests/Hedgehog.Tests/PropertyTests.fs
@@ -6,11 +6,11 @@ open TestDsl
 
 let propertyTests = testList "Property tests" [
     fableIgnore "generated C# list of five elements is not abbreviated in the failure report" <| fun _ ->
-        let prop = property {
+        let report = (property {
             let! xs = Range.singleton 0 |> Gen.int |> Gen.list (Range.singleton 5) |> Gen.map ResizeArray
             return false
         }
-        let report = Property.renderWith (PropertyConfig.withShrinks 0<shrinks> PropertyConfig.defaultConfig) prop
+        |> Property.renderWith (PropertyConfig.withShrinks 0<shrinks> PropertyConfig.defaultConfig))
         Expect.isNotMatch report "\.\.\." "Abbreviation (...) found"
 
 ]
