111bd98201081b48936aed42e371bead379a8b1b
Adam Becker <im.adam.becker@gmail.com>
2021-09-06 18:42:03 +0000

    Inlined 'Random.delay'

--- src/Hedgehog/Gen.fs
+++ src/Hedgehog/Gen.fs
@@ -1,6 +1,98 @@
 ﻿namespace Hedgehog
 
 open System
+open Hedgehog.Numeric
+
+/// A generator for random values of type 'a
+[<Struct>]
+type Random<'a> =
+    | Random of (Seed -> Size -> 'a)
+
+module Random =
+    let internal unsafeRun (seed : Seed) (size : Size) (Random r : Random<'a>) : 'a =
+        r seed size
+
+    let run (seed : Seed) (size : Size) (r : Random<'a>) : 'a =
+        unsafeRun seed (max 1 size) r
+
+    let tryFinally (after : unit -> unit) (r : Random<'a>) : Random<'a> =
+        Random (fun seed size ->
+            try
+                unsafeRun seed size r
+            finally
+                after ())
+
+    let tryWith (k : exn -> Random<'a>) (r : Random<'a>) : Random<'a> =
+        Random (fun seed size ->
+            try
+                unsafeRun seed size r
+            with
+                x -> unsafeRun seed size (k x))
+
+    let constant (x : 'a) : Random<'a> =
+        Random (fun _ _ -> x)
+
+    let map (f : 'a -> 'b) (r : Random<'a>) : Random<'b> =
+        Random (fun seed size ->
+            r
+            |> unsafeRun seed size
+            |> f)
+
+    let bind (k : 'a -> Random<'b>) (r : Random<'a>) : Random<'b> =
+        Random (fun seed size ->
+            let seed1, seed2 = Seed.split seed
+            r
+            |> unsafeRun seed1 size
+            |> k
+            |> unsafeRun seed2 size)
+
+    let replicate (times : int) (r : Random<'a>) : Random<List<'a>> =
+        Random (fun seed0 size ->
+            let rec loop seed k acc =
+                if k <= 0 then
+                    acc
+                else
+                    let seed1, seed2 = Seed.split seed
+                    let x = unsafeRun seed1 size r
+                    loop seed2 (k - 1) (x :: acc)
+            loop seed0 times [])
+
+    type Builder internal () =
+        member __.Return(x : 'a) : Random<'a> =
+            constant x
+        member __.ReturnFrom(m : Random<'a>) : Random<'a> =
+            m
+        member __.Bind(m : Random<'a>, k : 'a -> Random<'b>) : Random<'b> =
+            m |> bind k
+
+    /// Used to construct generators that depend on the size parameter.
+    let sized (f : Size -> Random<'a>) : Random<'a> =
+        Random (fun seed size ->
+            unsafeRun seed size (f size))
+
+    /// Overrides the size parameter. Returns a generator which uses the
+    /// given size instead of the runtime-size parameter.
+    let resize (newSize : Size) (r : Random<'a>) : Random<'a> =
+        Random (fun seed _ ->
+          run seed newSize r)
+
+    /// Generates a random integral number in the given inclusive range.
+    let inline integral (range : Range<'a>) : Random<'a> =
+        Random (fun seed size ->
+            let (lo, hi) = Range.bounds size range
+            let x, _ = Seed.nextBigInt (toBigInt lo) (toBigInt hi) seed
+            fromBigInt x)
+
+    /// Generates a random floating point number in the given inclusive range.
+    let inline double (range : Range<double>) : Random<double> =
+        Random (fun seed size ->
+            let (lo, hi) = Range.bounds size range
+            let x, _ = Seed.nextDouble lo hi seed
+            x)
+
+[<AutoOpen>]
+module RandomBuilder =
+    let random = Random.Builder ()
 
 /// A generator for values and shrink trees of type 'a.
 [<Struct>]
@@ -16,7 +108,10 @@ module Gen =
         r
 
     let delay (f : unit -> Gen<'a>) : Gen<'a> =
-        Random.delay (toRandom << f) |> ofRandom
+        let delay =
+            Random (fun seed size -> Random.unsafeRun seed size (toRandom (f ())))
+
+        ofRandom delay
 
     let tryFinally (after : unit -> unit) (m : Gen<'a>) : Gen<'a> =
         toRandom m |> Random.tryFinally after |> ofRandom
@@ -31,7 +126,7 @@ module Gen =
         Tree.singleton x |> Random.constant |> ofRandom
 
     let private bindRandom (k : 'a -> Random<Tree<'b>>) (m : Random<Tree<'a>>) : Random<Tree<'b>> =
-        Hedgehog.Random (fun seed0 size ->
+        Random (fun seed0 size ->
             let seed1, seed2 =
                 Seed.split seed0
 
@@ -286,8 +381,7 @@ module Gen =
             |> tryFilterRandom p
             |> Random.bind (function
                 | None ->
-                    Random.sized (fun n ->
-                        Random.resize (n + 1) (Random.delay loop))
+                    Random.sized (fun n -> Random (fun seed _ -> Random.unsafeRun seed (n + 1) (loop ())))
                 | Some x ->
                     Random.constant x)
 
