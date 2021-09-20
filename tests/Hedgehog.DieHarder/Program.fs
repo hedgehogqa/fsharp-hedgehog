c8ef5536334526c00f8167074597213221c0476a
Adam Becker <adam.becker@wwt.com>
2021-09-20 23:17:28 +0000

    Fix dieharder tests.

--- tests/Hedgehog.DieHarder/Program.fs
+++ tests/Hedgehog.DieHarder/Program.fs
@@ -1,10 +1,16 @@
-﻿open Hedgehog
+open System
+open System.IO
+open Hedgehog
 
 [<EntryPoint>]
 let main _ =
+    use stdout = Console.OpenStandardOutput()
+    use writer = new BinaryWriter(stdout)
+
     let rec loop seed =
         let (n, seed) = Seed.nextUInt64 seed
-        printfn "%d" n
+        writer.Write(n)
         loop seed
+
     loop (Seed.random ())
     0
