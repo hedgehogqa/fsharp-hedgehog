a0c38cfca4241ff3fbbf9c3e3b2b3822ed063ef8
Adam Becker <adam.becker@wwt.com>
2021-09-23 21:09:35 +0000

    Add FsCheck as a point of comparison.

--- tests/Hedgehog.Benchmarks/GenBenchmarks.fs
+++ tests/Hedgehog.Benchmarks/GenBenchmarks.fs
@@ -3,16 +3,24 @@ namespace Hedgehog.Benchmarks
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Jobs
 
-open Hedgehog
+module HRange = Hedgehog.Range
+module HGen = Hedgehog.Gen
+module FGen = FsCheck.Gen
 
 [<SimpleJob(RuntimeMoniker.NetCoreApp31)>]
 type GenBenchmarks () =
 
     [<Benchmark>]
-    member __.GenSampleLarge () =
-        let check () =
-            Range.constant -100 100
-            |> Gen.int32
-            |> Gen.sample 100 100000
-            |> Seq.iter ignore
-        check ()
+    member __.GenSampleLargeHedgehog () =
+        HRange.constant -100 100
+        |> HGen.int32
+        |> HGen.sample 100 100000
+        |> Seq.iter ignore
+
+    [<Benchmark>]
+    member __.GenSampleLargeFsCheck () =
+        ['0'..'9'] @ ['A'..'Z'] @ ['a'..'z']
+        |> FGen.elements
+        |> FGen.listOfLength 10
+        |> FGen.map (List.toArray >> System.String.Concat)
+        |> FGen.sample 0 10000
