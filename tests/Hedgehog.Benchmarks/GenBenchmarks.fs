412d466f78b77f8a2111cb9ba7931b1d95d19954
Adam Becker <im.adam.becker@gmail.com>
2021-09-24 00:34:21 +0000

    Fixed benchmarks and used better names.

--- tests/Hedgehog.Benchmarks/GenBenchmarks.fs
+++ tests/Hedgehog.Benchmarks/GenBenchmarks.fs
@@ -5,22 +5,21 @@ open BenchmarkDotNet.Jobs
 
 module HRange = Hedgehog.Range
 module HGen = Hedgehog.Gen
+module FArb = FsCheck.Arb
 module FGen = FsCheck.Gen
 
 [<SimpleJob(RuntimeMoniker.NetCoreApp31)>]
 type GenBenchmarks () =
 
     [<Benchmark>]
-    member __.GenSampleLargeHedgehog () =
+    member __.Hedgehog_Gen_Sample_Int32_100000 () =
         HRange.constant -100 100
         |> HGen.int32
         |> HGen.sample 100 100000
         |> Seq.iter ignore
 
     [<Benchmark>]
-    member __.GenSampleLargeFsCheck () =
-        ['0'..'9'] @ ['A'..'Z'] @ ['a'..'z']
-        |> FGen.elements
-        |> FGen.listOfLength 10
-        |> FGen.map (List.toArray >> System.String.Concat)
-        |> FGen.sample 0 10000
+    member __.FsCheck_Gen_Sample_Int32_100000 () =
+        FArb.generate<int>
+        |> FGen.sample 100 100000
+        |> Seq.iter ignore
