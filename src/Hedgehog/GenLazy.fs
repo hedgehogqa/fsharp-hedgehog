dc321d25f2780dbe436ccd013dcf986491790950
Tyson Williams <tyson.d.w@gmail.com>
2021-12-07 19:49:01 -0600

    Add some functional programming primitives related to Lazy

---
+++ src/Hedgehog/GenLazy.fs
@@ -0,0 +1,15 @@
+﻿// Workaround for a Fable issue: https://github.com/fable-compiler/Fable/issues/2069
+#if FABLE_COMPILER
+module Hedgehog.GenLazy
+#else
+[<RequireQualifiedAccess>]
+module internal Hedgehog.GenLazy
+#endif
+
+let constant a = a |> Lazy.constant |> Gen.constant
+
+let map f = f |> Lazy.map |> Gen.map
+
+let join glgla = glgla |> Gen.bind Lazy.value
+
+let bind f gla = gla |> map f |> join
---
+++ src/Hedgehog/Lazy.fs
@@ -0,0 +1,24 @@
+﻿// Workaround for a Fable issue: https://github.com/fable-compiler/Fable/issues/2069
+#if FABLE_COMPILER
+module Hedgehog.Lazy
+#else
+[<RequireQualifiedAccess>]
+module internal Hedgehog.Lazy
+#endif
+
+let func (f: unit -> 'a) = Lazy<'a>(valueFactory = fun () -> f ())
+
+let constant (a: 'a) = Lazy<'a>(valueFactory = fun () -> a)
+
+let value (ma: Lazy<'a>) = ma.Value
+
+let map (f: 'a -> 'b) (ma: Lazy<'a>) : Lazy<'b> =
+    (fun () -> ma.Value |> f)
+    |> func
+
+let join (mma: Lazy<Lazy<'a>>) =
+    (fun () -> mma.Value.Value)
+    |> func
+
+let bind (f: 'a -> Lazy<'b>) =
+    f |> map >> join
