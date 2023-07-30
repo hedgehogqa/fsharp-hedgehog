2afe91121bf906474de1c9edfe71bc8c79341b25
tom <leko.tomas@fronius.com>
2023-07-30 22:35:15 +0200

    add workaround for fable decimal bug
    See issue here: https://github.com/fable-compiler/Fable/issues/3500

--- tests/Hedgehog.Tests/RangeTests.fs
+++ tests/Hedgehog.Tests/RangeTests.fs
@@ -224,9 +224,6 @@ let rangeTests = testList "Range tests" [
         let actual =
             (Range.exponentialBounded () : Range<decimal>)
             |> Range.bounds 99
-// TODO: should also work for Fable
-#if !FABLE_COMPILER
+
         actual =! (Decimal.MinValue, Decimal.MaxValue)
-#endif
-        ()
 ]
