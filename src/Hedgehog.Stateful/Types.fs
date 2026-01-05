5a6f14462574d0e4514463312eb59ad1cf18f9b7
Alexey Raga <alexey.raga@gmail.com>
2026-01-05 11:48:35 +1100

    Custom equality for Env

--- src/Hedgehog.Stateful/Types.fs
+++ src/Hedgehog.Stateful/Types.fs
@@ -25,6 +25,7 @@ type Env = private {
 /// A symbolic variable is not yet bound to a generated value and is used to represent a value in the model before binding occurs.
 /// </summary>
 [<StructuredFormatDisplay("{DisplayText}")>]
+[<CustomEquality; CustomComparison>]
 type Var<'T> = private {
     /// <summary>
     /// The unique integer name of the variable.
@@ -126,6 +127,20 @@ Commands that use Var<T> inputs must override Require to call TryResolve and ret
 
     static member internal CreateSymbolic(value: 'T) : Var<'T> =
         { Name = -1; Default = Some value; Transform = unbox<'T>; ResolvedValue = None }
+
+    override this.Equals(other: obj) : bool =
+        match other with
+        | :? Var<'T> as otherVar -> this.Name = otherVar.Name
+        | _ -> false
+
+    override this.GetHashCode() : int =
+        hash this.Name
+
+    interface System.IComparable with
+        member this.CompareTo(other: obj) : int =
+            match other with
+            | :? Var<'T> as otherVar -> compare this.Name otherVar.Name
+            | _ -> invalidArg (nameof other) "Cannot compare values of different types"
 
 
 module internal Env =
